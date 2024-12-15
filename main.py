1a8a9b9d648631e2d290d495149e28f6f5c3053c
Jurand76 <jurand@interia.pl>
2024-12-15 01:15:54 +0100

    training model fixed

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
 from transformers import T5Tokenizer, T5ForConditionalGeneration, Trainer, TrainingArguments, DataCollatorForSeq2Seq
+from datasets import Dataset
 
-# Inicjalizacja modelu i tokenizera
-model_name = "t5-small"
-tokenizer = T5Tokenizer.from_pretrained(model_name, legacy=False)
-model = T5ForConditionalGeneration.from_pretrained(model_name)
+# Dane treningowe i walidacyjne
+train_data = [
+    {"input": ["Pokaż", "wszystkie", "samochody", "wyprodukowane", "po", "2015", "roku."],
+     "output": ["SELECT", "*", "FROM", "cars", "WHERE", "year", ">", "2015;"]},
+    {"input": ["Wymień", "marki", "i", "modele", "samochodów", "z", "rokiem", "2020."],
+     "output": ["SELECT", "make,", "model", "FROM", "cars", "WHERE", "year", "=", "2020;"]}
+]
 
-# Przetwarzanie danych
+eval_data = [
+    {"input": ["Ile", "samochodów", "wyprodukowano", "w", "2015", "roku?"],
+     "output": ["SELECT", "COUNT(*)", "FROM", "cars", "WHERE", "year", "=", "2015;"]},
+    {"input": ["Pokaż", "wszystkie", "samochody", "marki", "Toyota."],
+     "output": ["SELECT", "*", "FROM", "cars", "WHERE", "make", "=", "'Toyota';"]}
+]
+
+# Napraw dane wejściowe
+def fix_input_output(data):
+    for example in data:
+        example["input"] = " ".join(example["input"]) if isinstance(example["input"], list) else example["input"]
+        example["output"] = " ".join(example["output"]) if isinstance(example["output"], list) else example["output"]
+    return data
+
+train_data = fix_input_output(train_data)
+eval_data = fix_input_output(eval_data)
+
+print("Train data")
+print(train_data)
+
+# Utwórz Dataset
+train_dataset = Dataset.from_list(train_data)
+eval_dataset = Dataset.from_list(eval_data)
+
+# Tokenizer i model
+tokenizer = T5Tokenizer.from_pretrained("t5-small", legacy=False)
+model = T5ForConditionalGeneration.from_pretrained("t5-small")
+
+# Funkcja przetwarzania danych
 def preprocess_function(examples):
     model_inputs = tokenizer(
         examples["input"],
@@ -22,34 +54,20 @@ def preprocess_function(examples):
     model_inputs["labels"] = labels["input_ids"]
     return model_inputs
 
-# Przygotowanie zbiorów danych
-from datasets import Dataset
+# Przetwórz dane
+print("Przetwarzanie train dataset")
+columns_to_remove = ["input", "output"]   # usuwanie kolumn input i output z datasetu
+train_dataset = train_dataset.map(preprocess_function, batched=False, remove_columns=columns_to_remove)
+print(train_dataset[0])
 
-data = [
-    {"input": "Pokaż wszystkie samochody wyprodukowane po 2015 roku.", "output": "SELECT * FROM cars WHERE year > 2015;"},
-    {"input": "Wymień marki i modele samochodów z rokiem 2020.", "output": "SELECT make, model FROM cars WHERE year = 2020;"}
-]
+print("Przetwarzanie eval dataset")
+eval_dataset = eval_dataset.map(preprocess_function, batched=False, remove_columns=columns_to_remove)
+print(eval_dataset[0])
 
-train_dataset = Dataset.from_list(data)
-
-for example in train_dataset:
-    print(example["input"], example["output"])
-
-eval_data = [
-    {"input": "Ile samochodów wyprodukowano w 2015 roku?", "output": "SELECT COUNT(*) FROM cars WHERE year = 2015;"},
-    {"input": "Pokaż wszystkie samochody marki Toyota.", "output": "SELECT * FROM cars WHERE make = 'Toyota';"}
-]
-
-eval_dataset = Dataset.from_list(eval_data)
-
-train_dataset = train_dataset.map(preprocess_function, batched=True)
-eval_dataset = eval_dataset.map(preprocess_function, batched=True)
-
-# Ustawienia trenera
+# Argumenty treningowe
 training_args = TrainingArguments(
     output_dir="./results",
-    evaluation_strategy="epoch",
-    save_strategy="epoch",
+    eval_strategy="epoch",  # Poprawiony parametr
     learning_rate=5e-5,
     per_device_train_batch_size=8,
     num_train_epochs=3,
@@ -58,10 +76,14 @@ training_args = TrainingArguments(
     remove_unused_columns=False
 )
 
-# Data collator dla przetwarzania sekwencji
-data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
+# Data collator
+data_collator = DataCollatorForSeq2Seq(
+    tokenizer=tokenizer,
+    model=model,
+    padding=True
+)
 
-# Inicjalizacja trenera
+# Trener
 trainer = Trainer(
     model=model,
     args=training_args,
